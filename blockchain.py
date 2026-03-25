38b3958a474b5066dbe064c4cae4f0b7918f744e
grossbaj <grossbaj@iu.edu>
2026-03-25 13:16:07 -0400

    Added a method for adding neighbouring nodes to out Network. We have used a set() to hold the list of nodes. This is a cheap way of ensuring that the addition of new nodes is idempotent, meaning that no matter how many times we add a specific node, it appears exactly once.

--- blockchain.py
+++ blockchain.py
@@ -6,14 +6,27 @@ from uuid import uuid4
 
 from flask import Flask, jsonify, request
 
+from urllib.parse import urlparse
+
 
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+        self.nodes = set()
 
         # Create the genesis block
         self.new_block(previous_hash=1, proof=100)
+
+    def register_node(self, address):
+        """
+        Add a new node to the list of nodes
+        :param address: <str> Address of node. Eg. 'http://192.168.0.5:5000'
+        :return: None
+        """
+
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
 
     def new_block(self, proof, previous_hash=None):
         """
@@ -37,7 +50,7 @@ class Blockchain(object):
         self.chain.append(block)
         return block
 
-    def new_transactions(self, sender, recipient, amount):
+    def new_transaction(self, sender, recipient, amount):
         """
         Creates a new transaction to go into the next mined Block
         :param sender: <str> Address of the Sender

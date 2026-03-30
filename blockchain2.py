0c49ce07ea073909362f73d1dd518af0127c2fae
grossbaj <grossbaj@iu.edu>
2026-03-30 03:20:58 -0400

    Creating a blockchain class that implements the following methods: register node: Adds a new blockchain node to the list of nodes. verify_transaction_signature: Checks that the provided signature corresponds to transaction signed by the public key. submit_transaction: Adds a transaction to list of transactions if the signature is verified. create_block: Adds a block of transactions to the blockchain. hash: Create a SHA-256 hash of a block. proof_of_work: Proof of work algorithm. Looks for a nonce that satisfies the mining condition. valid_proof: Checks if a hash value satisfies the mining conditions. This function is used within the proof_of_work function. valid_chain: Checks if a blockchain is valid. resolve_conflicts: Resolves conflicts between blockchain's nodes by replacing a chain with the longest one in the network.

---
+++ blockchain2.py
@@ -0,0 +1,77 @@
+from uuid import uuid4
+from flask import Flask, render_template
+
+class Blockchain:
+
+    def __init__(self):
+        self.transactions = []
+        self.chain = []
+        self.nodes = set()
+        # Generate random number to be used as node_id
+        self.node_id = str(uuid4()).replace('-', '')
+        # Create genesis block
+        self.create_block(0, '00')
+
+    def register_node(self, node_url):
+        """
+        Add a new node to the list of nodes
+        """
+        ...
+
+    def verify_transaction_signature(self, sender_address, signature, transaction):
+        """
+        Check that the provided signature corresponds to transaction
+        signed by the public key (sender_address)
+        """
+        ...
+
+    def submit_transaction(self, sender_address, recipient_address, value, signature):
+        """
+        Add a transaction array if the signature verified
+        """
+        ...
+
+    def create_block(self, nonce, previous_hash):
+        """
+        Add a block of transactions to the blockchain
+        """
+        ...
+
+    def hash(self, block):
+        """
+        Create a SHA-256 hash of a block
+        """
+
+    def proof_of_work(self):
+        """
+        Proof of work algorithm
+        """
+
+    def valid_proof(self, transactions, last_hash, nonce, difficulty=MINING_DIFFICULTY):
+        """
+        Check if a hash value satisfies the mining conditions. This function is used within the proof_of_work function
+        """
+        ...
+
+    def valid_chain(self, chain):
+        """
+        Check if a blockchain is valid
+        """
+        ...
+    
+    def resolve_conflicts(self):
+        """
+        Resolve conflicts between blockchain's nodes
+        by replacing out chain with the longest one in the network.
+        """
+        ...
+    
+
+app = Flask(__name__)
+CORS(app)
+
+blockchain = Blockchain()
+
+@app.route('/')
+def index():
+    return render_template('./index.html')
